d0d14fe855be466ded0307d3dc1593e85e9d8914
dheeraj-1024 <95970948+dheeraj-1024@users.noreply.github.com>
2022-05-20 20:13:05 +0530

    Create cnn_simple.py

---
+++ cnn_simple.py
@@ -0,0 +1,23 @@
+#                   *******************************
+#                   *     IMPLEMENTING A CNN      *
+#                   *******************************
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (Dense,Conv2D,MaxPool2D,Flatten,Dropout,BatchNormalization,)
+
+model = Sequential()
+model.add(Conv2D(75, (3, 3), strides=1, padding="same", activation="relu", 
+                 input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Conv2D(50, (3, 3), strides=1, padding="same", activation="relu"))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Conv2D(25, (3, 3), strides=1, padding="same", activation="relu"))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Flatten())
+model.add(Dense(units=512, activation="relu"))
+model.add(Dropout(0.3))
+model.add(Dense(units=num_classes, activation="softmax"))
